b2ec7b8352de488d38ebed2dbfa40686b7a5a4f2
Adam <adam.noszczynski@gmail.com>
2020-07-12 15:07:51 +0200

    merge fix

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,21 @@
 const objectToArray = (obj) => {
-  const arr = []
-  Object.keys(obj).map(
-    key => arr.push(obj[key])
-  )
-  return arr
-}
+  const arr = [];
+  Object.keys(obj).map((key) => arr.push(obj[key]));
+  return arr;
+};
 
-export {objectToArray}
+const filterArrayByFields = (term, arr = [], fields = []) => {
+  const smallerTerm = term.toLowerCase();
+  let searchString = "";
+
+  return arr.filter((item) => {
+    searchString = "";
+    fields.forEach((field) => {
+      searchString += item[field].toString();
+    });
+
+    return searchString.toLowerCase().search(smallerTerm) !== -1;
+  });
+};
+
+export { objectToArray, filterArrayByFields };
